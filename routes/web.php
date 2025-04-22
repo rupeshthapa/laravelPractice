9420c72a66b05395e2b22280d64e1f2b0b5f5f5a
rupeshthapa <leleventuresadv@gmail.com>
2025-04-22 11:30:15 +0545

    Initial Commit

---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Http\Controllers\FormController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PageController;
+use Illuminate\Support\Facades\Session;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+use function Laravel\Prompts\form;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+// Route::get('/{id?}/{name?}', [PageController::class, 'home'])->name('home');
+// Route::get('/about', [PageController::class, 'about'])->name('about');
+// Route::get('/contact', [PageController::class, 'contact'])->name('contact');
+
+// Route::get('/hello/{name}', function($name){
+//     return view('hello', ['name'=> $name]);
+// });
+
+Route::get('/', function(){
+    return view('login');
+});
